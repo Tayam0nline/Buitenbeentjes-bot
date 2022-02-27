61a095d5af973241e206431c7971e630b244f380
SemPlaatsman <688208@student.inholland.nl>
2022-02-27 17:06:49 +0100

    Added >ping command

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 const Discord = require("discord.js");
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
-const prefix = "-";
+const prefix = ">";
 
 client.once("ready", () => {
   console.log("Daan is noob");
 });
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.startsWith(prefix)) return;
+client.on("message", (message) => {
+  //console.log(message);
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  if (command === "!ping") {
-    await interaction.reply("Pong!");
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+  //console.log(command);
+
+  if (command === "ping") {
+    message.channel.send("pong!");
+  }
+
+  if (message.author.username === "Colin Kroket") {
+    message.channel.send();
   }
 });
 
