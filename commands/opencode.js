b841b97b14e9554240e944cc5f84e772b32bae71
Tayam0nline <89915786+Tayam0nline@users.noreply.github.com>
2022-03-01 12:20:45 +0100

    update OpenCode
    OpenCode with actual random chance to get rickrolled

--- commands/opencode.js
+++ commands/opencode.js
@@ -1,7 +1,14 @@
 module.exports = {
   name: "opencode",
   description: "opencode command that rick rolls anyone that executes it",
+  import java.util.Random;
+Random rand = new Random();
+int n = rand.nextInt(11);
+if (n == 1){
+    execute(message, args) {
+    message.channel.send("https://c.tenor.com/VFFJ8Ei3C2IAAAAM/rickroll-rick.gif"); }
+else{
   execute(message, args) {
-    message.channel.send("https://github.com/SemPlaatsman/Buitenbeentjes-bot");
+    message.channel.send("https://github.com/SemPlaatsman/Buitenbeentjes-bot");}
   },
 };
