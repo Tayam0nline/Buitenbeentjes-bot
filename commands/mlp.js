6fd7d4c64fd9be25cee1d74ca04fa4a3ffe429e0
SemPlaatsman <688208@student.inholland.nl>
2022-03-01 11:49:28 +0100

    Added extra discord js packages

---
+++ commands/mlp.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: "mlp",
+  description: "Please don't look at this code",
+  execute(message, args) {
+    message.channel.send(`<@&${938569064786305064}>\nBRONIES ASSEMBLE!!!`);
+  },
+};
